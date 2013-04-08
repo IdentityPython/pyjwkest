7ada403434673b51be5bbd1f7e5ab3ac78082a88
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-04-08 13:51:36 +0200

    Accept padded base64 even though it's frowned upon.

--- src/jwkest/__init__.py
+++ src/jwkest/__init__.py
@@ -15,9 +15,11 @@ logger = logging.getLogger(__name__)
 
 JWT_TYPES = (u"JWT", u"application/jws", u"JWS", u"JWE")
 
+
 # XXX Should this be a subclass of ValueError?
 class Invalid(Exception):
     """The JWT is invalid."""
+
 
 class BadSyntax(Invalid):
     """The JWT could not be parsed because the syntax is invalid."""
@@ -29,20 +31,26 @@ class BadSyntax(Invalid):
     def __str__(self):
         return "%s: %r" % (self.msg, self.value)
 
+
 class BadSignature(Invalid):
     """The signature of the JWT is invalid."""
+
 
 class Expired(Invalid):
     """The JWT claim has expired or is not yet valid."""
 
+
 class UnknownAlgorithm(Invalid):
     """The JWT uses an unknown signing algorithm"""
+
 
 class BadType(Invalid):
     """The JWT has an unexpected "typ" value."""
 
+
 class MissingKey(Exception):
     """ No usable key """
+
 
 def b64e(b):
     """Base64 encode some bytes.
@@ -51,16 +59,9 @@ def b64e(b):
     return base64.urlsafe_b64encode(b).rstrip(b"=")
 
 _b64_re = re.compile(b"^[A-Za-z0-9_-]*$")
-def b64d(b):
-    """Decode some base64-encoded bytes.
 
-    Raises BadSyntax if the string contains invalid characters or padding."""
 
-    # Python's base64 functions ignore invalid characters, so we need to
-    # check for them explicitly.
-    if not _b64_re.match(b):
-        raise BadSyntax(b, "base64-encoded data contains illegal characters")
-
+def add_padding(b):
     # add padding chars
     m = len(b) % 4
     if m == 1:
@@ -71,12 +72,37 @@ def b64d(b):
         b += b"=="
     elif m == 3:
         b += b"="
+    return b
+
+
+def b64d(b):
+    """Decode some base64-encoded bytes.
+
+    Raises BadSyntax if the string contains invalid characters or padding."""
+
+    if b.endswith("="):  # shouldn't but there you are
+        cb = b.split("=")[0]
+    else:
+        cb = b
+
+    # Python's base64 functions ignore invalid characters, so we need to
+    # check for them explicitly.
+    if not _b64_re.match(cb):
+        raise BadSyntax(cb, "base64-encoded data contains illegal characters")
+
+    if cb == b:
+        b = add_padding(b)
+
     return base64.urlsafe_b64decode(b)
+
 
 def split_token(token):
     if token.count(b".") != 2:
-        raise BadSyntax(token, "expected token to contain 2 dots, not %d" % token.count(b"."))
+        raise BadSyntax(token,
+                        "expected token to contain 2 dots, not %d" %
+                        token.count(b"."))
     return tuple(token.split(b"."))
+
 
 # Stolen from Werkzeug
 def safe_str_cmp(a, b):
@@ -87,6 +113,7 @@ def safe_str_cmp(a, b):
     for c, d in izip(a, b):
         r |= ord(c) ^ ord(d)
     return r == 0
+
 
 def unpack(token):
     """
@@ -109,6 +136,7 @@ def unpack(token):
 
     return header, claim, crypto, header_b64, claim_b64
 
+
 def pack(payload):
     """
     Unsigned JWT
@@ -127,16 +155,21 @@ def pack(payload):
 
 # ---------------------------------------------------------------------------
 # Helper functions
+
+
 def intarr2bin(arr):
     return unhexlify(''.join(["%02x" % byte for byte in arr]))
+
 
 def intarr2long(arr):
     return long(''.join(["%02x" % byte for byte in arr]), 16)
 
+
 def hd2ia(s):
     #half = len(s)/2
-    return [int(s[i]+s[i+1], 16) for i in range(0,  len(s), 2)]
+    return [int(s[i] + s[i + 1], 16) for i in range(0, len(s), 2)]
+
 
 def dehexlify(bi):
     s = hexlify(bi)
-    return [int(s[i]+s[i+1], 16) for i in range(0,len(s),2)]
+    return [int(s[i] + s[i + 1], 16) for i in range(0, len(s), 2)]
