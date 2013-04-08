38a7e5d05f766fc8b5fb370c5d66358c4b5dca2c
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-04-08 13:53:20 +0200

    Editorial plus added som new tests.

--- tests/test_0_jwk.py
+++ tests/test_0_jwk.py
@@ -18,17 +18,20 @@ __author__ = 'rohe0002'
 CERT = "certs/cert.pem"
 KEY = "certs/server.key"
 
-JWK = {"keys":[{'kty': 'RSA',
-                'use': 'foo',
-                'e': 'AQAB',
-                'n': 'wf-wiusGhA-gleZYQAOPQlNUIucPiqXdPVyieDqQbXXOPBe3nuggtVzeq7pVFH1dZz4dY2Q2LA5DaegvP8kRvoSB_87ds3dy3Rfym_GUSc5B0l1TgEobcyaep8jguRoHto6GWHfCfKqoUYZq4N8vh4LLMQwLR6zi6Jtu82nB5k8'}]}
+JWK = {"keys": [{'kty': 'RSA',
+                 'use': 'foo',
+                 'e': 'AQAB',
+                 'n': 'wf-wiusGhA-gleZYQAOPQlNUIucPiqXdPVyieDqQbXXOPBe3nuggtVzeq7pVFH1dZz4dY2Q2LA5DaegvP8kRvoSB_87ds3dy3Rfym_GUSc5B0l1TgEobcyaep8jguRoHto6GWHfCfKqoUYZq4N8vh4LLMQwLR6zi6Jtu82nB5k8'}]}
+
 
 def _eq(l1, l2):
     return set(l1) == set(l2)
 
+
 def test_x509_rsa_loads():
     _ckey = x509_rsa_loads(open(CERT).read())
     assert isinstance(_ckey, M2Crypto.RSA.RSA_pub)
+
 
 def test_x509_rsa_loads_2():
     _ckey = x509_rsa_loads(open(CERT).read())
@@ -45,6 +48,7 @@ def test_x509_rsa_loads_2():
 
     assert cn == jn
 
+
 def test_kspec():
     _ckey = x509_rsa_loads(open(CERT).read())
     _jwk = kspec(_ckey)
@@ -52,6 +56,7 @@ def test_kspec():
     assert _jwk["kty"] == "RSA"
     assert _jwk["e"] == JWK["keys"][0]["e"]
     assert _jwk["n"] == JWK["keys"][0]["n"]
+
 
 def test_loads_0():
 
@@ -66,8 +71,9 @@ def test_loads_0():
     assert key.n == _ckey.n
     assert key.e == _ckey.e
 
+
 def test_loads_1():
-    JWK = {"keys":[{'kty': 'RSA',
+    JWK = {"keys": [{'kty': 'RSA',
                     'use': 'foo',
                     'e': 'AQAB',
                     "n": 'wf-wiusGhA-gleZYQAOPQlNUIucPiqXdPVyieDqQbXXOPBe3nuggtVzeq7pVFH1dZz4dY2Q2LA5DaegvP8kRvoSB_87ds3dy3Rfym_GUSc5B0l1TgEobcyaep8jguRoHto6GWHfCfKqoUYZq4N8vh4LLMQwLR6zi6Jtu82nB5k8',
@@ -82,13 +88,15 @@ def test_loads_1():
     keys = load_jwks(json.dumps(JWK))
     print keys
     assert len(keys) == 2
-    key_ids = [id for id,key in keys]
+    key_ids = [i for i,key in keys]
     assert _eq(key_ids, ["rsa:1", "rsa:2"])
+
 
 def test_dumps():
     _ckey = x509_rsa_loads(open(CERT).read())
     jwk = dump_jwk(_ckey)
     assert _eq(jwk.keys(), ["kty", "e", "n"])
+
 
 def test_dump_jwk():
     _ckey = x509_rsa_loads(open(CERT).read())
@@ -98,6 +106,7 @@ def test_dump_jwk():
     assert _wk.keys() == ["keys"]
     assert len(_wk["keys"]) == 1
     assert _eq(_wk["keys"][0].keys(), ["kty", "e", "n"])
+
 
 def test_load_jwk():
     _ckey = x509_rsa_loads(open(CERT).read())
@@ -109,6 +118,7 @@ def test_load_jwk():
     assert typ == "rsa"
     assert isinstance(key, M2Crypto.RSA.RSA)
 
+
 def test_rsa_load():
     _ckey = rsa_load(KEY)
     assert isinstance(_ckey, M2Crypto.RSA.RSA)
@@ -118,6 +128,7 @@ def test_rsa_load():
     assert jwk["n"] == '5zbNbHIYIkGGJ3RGdRKkYmF4gOorv5eDuUKTVtuu3VvxrpOWvwnFV-NY0LgqkQSMMyVzodJE3SUuwQTUHPXXY5784vnkFqzPRx6bHgPxKz7XfwQjEBTafQTMmOeYI8wFIOIHY5i0RWR-gxDbh_D5TXuUqScOOqR47vSpIbUH-nc'
     assert jwk['e'] == 'AQAB'
 
+
 def test_rsa_loads():
     _ckey = rsa_loads(open(KEY).read())
     assert isinstance(_ckey, M2Crypto.RSA.RSA)
@@ -126,4 +137,3 @@ def test_rsa_loads():
     assert _eq(jwk.keys(), ["kty", "e", "n"])
     assert jwk["n"] == '5zbNbHIYIkGGJ3RGdRKkYmF4gOorv5eDuUKTVtuu3VvxrpOWvwnFV-NY0LgqkQSMMyVzodJE3SUuwQTUHPXXY5784vnkFqzPRx6bHgPxKz7XfwQjEBTafQTMmOeYI8wFIOIHY5i0RWR-gxDbh_D5TXuUqScOOqR47vSpIbUH-nc'
     assert jwk['e'] == 'AQAB'
-
--- tests/test_1_jws.py
+++ tests/test_1_jws.py
@@ -1,30 +1,32 @@
-import json
-#from oic.utils.keystore import rsa_load
+from jwkest.jws import SIGNER_ALGS, verify
 
 __author__ = 'rohe0002'
 
 import jwkest
 from jwkest import jws
-from jwkest.jwk import load_jwks, x509_rsa_loads
+from jwkest import b64e
 
-CERT = "certs/cert.pem"
-KEY  = "certs/server.key"
+#CERT = "certs/cert.pem"
+KEY = "certs/server.key"
+
 
 def rsa_load(filename):
     """Read a PEM-encoded RSA key pair from a file.
-        - same code as : https://github.com/rohe/pyoidc/blob/master/src/oic/utils/keyio.py
+        - same code as :
+            https://github.com/rohe/pyoidc/blob/master/src/oic/utils/keyio.py
     """
     import M2Crypto
     return M2Crypto.RSA.load_key(filename, M2Crypto.util.no_passphrase_callback)
 
-JWK = {"keys":[{'alg': 'RSA',
-                'use': 'foo',
-                'xpo': 'AQAB',
-                'mod': 'wf-wiusGhA-gleZYQAOPQlNUIucPiqXdPVyieDqQbXXOPBe3nuggtVzeq7pVFH1dZz4dY2Q2LA5DaegvP8kRvoSB_87ds3dy3Rfym_GUSc5B0l1TgEobcyaep8jguRoHto6GWHfCfKqoUYZq4N8vh4LLMQwLR6zi6Jtu82nB5k8'}]}
+JWK = {"keys": [{'alg': 'RSA',
+                 'use': 'foo',
+                 'xpo': 'AQAB',
+                 'mod': 'wf-wiusGhA-gleZYQAOPQlNUIucPiqXdPVyieDqQbXXOPBe3nuggtVzeq7pVFH1dZz4dY2Q2LA5DaegvP8kRvoSB_87ds3dy3Rfym_GUSc5B0l1TgEobcyaep8jguRoHto6GWHfCfKqoUYZq4N8vh4LLMQwLR6zi6Jtu82nB5k8'}]}
+
 
 def test_1():
-    claimset = {"iss":"joe",
-                "exp":1300819380,
+    claimset = {"iss": "joe",
+                "exp": 1300819380,
                 "http://example.com/is_root": True}
 
     _jwt = jwkest.pack(claimset)
@@ -34,6 +36,7 @@ def test_1():
     assert part[0] == {u'alg': u'none'}
     assert part[1] == \
            '{"iss":"joe","exp":1300819380,"http://example.com/is_root":true}'
+
 
 def test_hmac_256():
     payload = "Please take a moment to register today"
@@ -45,6 +48,7 @@ def test_hmac_256():
 
     assert info == payload
 
+
 def test_hmac_384():
     payload = "Please take a moment to register today"
     keycol = {"hmac": "My hollow echo"}
@@ -54,6 +58,7 @@ def test_hmac_384():
     info = jws.verify(_jwt, keycol)
 
     assert info == payload
+
 
 def test_hmac_512():
     payload = "Please take a moment to register today"
@@ -65,16 +70,19 @@ def test_hmac_512():
 
     assert info == payload
 
+
 def test_left_hash_hs256():
     hsh = jws.left_hash("Please take a moment to register today")
     assert hsh == "rCFHVJuxTqRxOsn2IUzgvA"
+
 
 def test_left_hash_hs512():
     hsh = jws.left_hash("Please take a moment to register today", "HS512")
     assert hsh == "_h6feWLt8zbYcOFnaBmekTzMJYEHdVTaXlDgJSWsEeY"
 
+
 def test_rs256():
-    rsapub = rsa_load(KEY )
+    rsapub = rsa_load(KEY)
 
     payload = "Please take a moment to register today"
     keycol = {"rsa": [rsapub]}
@@ -84,6 +92,7 @@ def test_rs256():
     info = jws.verify(_jwt, keycol)
 
     assert info == payload
+
 
 def test_rs384():
     rsapub = rsa_load(KEY)
@@ -97,6 +106,7 @@ def test_rs384():
 
     assert info == payload
 
+
 def test_rs512():
     rsapub = rsa_load(KEY)
 
@@ -108,3 +118,49 @@ def test_rs512():
     info = jws.verify(_jwt, keycol)
 
     assert info == payload
+
+
+def test_a_1_1a():
+    header = '{"typ":"JWT",\r\n "alg":"HS256"}'
+    val = b64e(header)
+    assert val == "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9"
+
+
+def test_a_1_1b():
+    payload = '{"iss":"joe",\r\n "exp":1300819380,\r\n "http://example.com/is_root":true}'
+    val = b64e(payload)
+    assert val == "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ"
+
+HMAC_KEY = [3, 35, 53, 75, 43, 15, 165, 188, 131, 126, 6, 101, 119, 123, 166,
+            143, 90, 179, 40, 230, 240, 84, 201, 40, 169, 15, 132, 178, 210, 80,
+            46, 191, 211, 251, 90, 146, 210, 6, 71, 239, 150, 138, 180, 195,
+            119, 98, 61, 34, 61, 46, 33, 114, 5, 46, 79, 8, 192, 205, 154, 245,
+            103, 208, 128, 163]
+
+
+def test_a_1_1c():
+
+    hmac = jwkest.intarr2bin(HMAC_KEY)
+    signer = SIGNER_ALGS["HS256"]
+    header = '{"typ":"JWT",\r\n "alg":"HS256"}'
+    payload = '{"iss":"joe",\r\n "exp":1300819380,\r\n "http://example.com/is_root":true}'
+    sign_input = b64e(header) + '.' + b64e(payload)
+    sig = signer.sign(sign_input, hmac)
+    assert b64e(sig) == "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
+
+
+def test_a_1_3a():
+    _jwt = "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
+
+    #keycol = {"hmac": jwkest.intarr2bin(HMAC_KEY)}
+    header, claim, crypto, header_b64, claim_b64 = jwkest.unpack(_jwt)
+
+    hmac = jwkest.intarr2bin(HMAC_KEY)
+    signer = SIGNER_ALGS["HS256"]
+    info = signer.verify(header_b64 + '.' + claim_b64, crypto, hmac)
+
+
+def test_a_1_3b():
+    _jwt = "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
+    keycol = {"hmac": [jwkest.intarr2bin(HMAC_KEY)]}
+    verify(_jwt, keycol)
