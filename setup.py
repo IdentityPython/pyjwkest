dd62724f5705e23672078107bdd75df9fe9f27e7
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-11-13 23:06:19 +0100

    Changed file mods

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class PyTest(TestCommand):
 
 setup(
     name="pyjwkest",
-    version="0.5.0beta",
+    version="0.5.0gamma",
     description="Python implementation of JWT, JWE, JWS and JWK",
     author="Roland Hedberg",
     author_email="roland.hedberg@adm.umu.se",
