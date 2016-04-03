8a757f78b9f49ec6e20bc82ebc42fe98b5e9e801
Roland Hedberg <roland.hedberg@adm.umu.se>
2016-04-03 17:48:08 +0200

    Complete move from Crypto to Cryptodome

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4"],
-    install_requires=["pycryptodome", "requests", "six", "future"],
+    install_requires=["pycryptodomex", "requests", "six", "future"],
     tests_require=['pytest'],
     zip_safe=False,
     scripts=glob.glob('script/*.py'),
